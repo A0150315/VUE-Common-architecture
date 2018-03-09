b2ed171f7c25f799e1b26eca7e2495f98013c5b1
ChinaMobile <574469551@qq.com>
2018-03-09 11:11:32 +0800

    答题页完成

--- wmpo/demo2/src/components/Topic.vue
+++ wmpo/demo2/src/components/Topic.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-      <p>{{current+1}}、{{topics[current]?topics[current].title:''}}</p>
-      <p>{{isMaster?'(请选择你心中的答案)':''}}</p>
+  <div class="topic">
+      <p class="topic__title">{{current+1}}、{{topics[current]?topics[current].title:''}}</p>
+      <p v-if="isMaster">(请选择你心中的答案)</p>
       <ul>
-          <li v-for="(item,index) of topics[current].options" :key="index">
-              {{item}}
+          <li v-for="(item,index) of topics[current].options" 
+              :key="index" class="topic__listitem" 
+              :class="{'top__listitem--selected':index===selectedIndexs[current]}"
+              @click="setSelectedIndex(index)" >
+                {{item}}
           </li>
       </ul>
   </div>
@@ -24,10 +27,34 @@ export default {
     isMaster: {
       type: Boolean,
       defalut: false
+    },
+    setSelectedIndex: {
+      type: Function,
+      defalut: () => console.log("请设置setSelectedIndex的函数")
+    },
+    selectedIndexs: {
+      type: Object
     }
-  },
-  mounted() {
-    console.log(this.topics[this.current]);
   }
 };
 </script>
+<style lang="postcss" scoped>
+.topic {
+  background-color: #fff;
+  border-bottom: 0.0075rem dashed #333;
+}
+.topic__title {
+  padding-bottom: 0.6rem;
+}
+.topic__listitem {
+  margin: 0.2rem;
+  padding: 0.26rem 0.16rem;
+  border-radius: 0.2rem;
+
+  font-weight: bold;
+  background-color: #f2f2f2;
+}
+.top__listitem--selected {
+  background-color: #fad852;
+}
+</style>
---
+++ wmpo/demo2/src/components/TopicButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="topbutton" :class="{'topbutton--unactive':!isFull}">提交</div>
+</template>
+<style lang="postcss" scoped>
+.topbutton {
+  width: 50vw;
+  height: 7vh;
+  border-radius: 0.1rem;
+
+  line-height: 7vh;
+  text-align: center;
+  background-color: #6b6b6b;
+  color: #fff;
+}
+.topbutton--unactive {
+  opacity: 0.4 !important;
+}
+</style>
+<script>
+export default {
+  props: {
+    current: {
+      type: [Number, String],
+      default: -1
+    },
+    isFull: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
