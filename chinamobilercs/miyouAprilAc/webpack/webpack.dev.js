8c3083c2e235ab55025432ed3e221495f368cd06
ChinaMobile <574469551@qq.com>
2018-03-15 18:30:24 +0800

    更新webpack

--- chinamobilercs/miyouAprilAc/webpack/webpack.dev.js
+++ chinamobilercs/miyouAprilAc/webpack/webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 const webpack = require("webpack");
 
@@ -12,7 +13,7 @@ module.exports = merge(common, {
     },
     devtool: "inline-source-map",
     devServer: {
-        contentBase: "./dist",
+        contentBase: path.join(__dirname, "../dist"),
         compress: false,
         port: 8888,
         hot: true
