7610c91f3d40b5361b6aa8177ce55a84843ea8d6
chenlong <13802885163@139.com>
2018-03-27 10:28:37 +0800

    修改整蛊记录页面的显示

--- chinamobilercs/miyouAprilAc/src/pages/record.vue
+++ chinamobilercs/miyouAprilAc/src/pages/record.vue
@@ -61,7 +61,7 @@
 					this.promptMsg = '您还没有挑战过整蛊呢';
 					this.mark = '1';
 					this.recordList = [];
-					this.loading = true;
+					this.loading = true;//防止切换标签时重复加载
 					this.getList();
 				} else {
 					this.pageNum = 1;
@@ -70,7 +70,7 @@
 					this.promptMsg = '您还没有整蛊过好友呢';
 					this.mark = '0';
 					this.recordList = [];
-					this.loading = true;
+					this.loading = true;//防止切换标签时重复加载
 					this.getList();
 				}
 			},
@@ -158,6 +158,12 @@
 		},
 		mounted() {
 			this.computeHeight();
+			//在这里判断从首页哪里点击进来的
+            if(this.$route.query.sign == '1'){
+			    this.isSelect = 0;
+				this.promptMsg = '您还没有挑战过整蛊呢';
+				this.mark = '1';
+            }
 		}
 	};
 </script>
